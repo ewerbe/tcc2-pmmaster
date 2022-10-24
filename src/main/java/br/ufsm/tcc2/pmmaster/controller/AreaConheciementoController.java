605a4f05e5dbf69709b545bf5260f6d36b5b4e03
Ewe Barbo <ewebarbo@gmail.com>
2022-10-24 15:32:36 -0300

    Controller de áreas de conhecimento inicializado.

---
+++ src/main/java/br/ufsm/tcc2/pmmaster/controller/AreaConheciementoController.java
@@ -0,0 +1,32 @@
+package br.ufsm.tcc2.pmmaster.controller;
+
+import br.ufsm.tcc2.pmmaster.model.AreaConhecimento;
+import br.ufsm.tcc2.pmmaster.model.Usuario;
+import br.ufsm.tcc2.pmmaster.service.AreaConhecimentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+public class AreaConheciementoController {
+
+    @Autowired
+    AreaConhecimentoService areaConhecimentoService;
+
+    @RequestMapping(value = "/areas-conhecimento.action", method = RequestMethod.GET)
+    public String getAreasConhecimento(Model model) {
+//                                   @RequestParam(value = "idUsu", required = false)Long idUsu) {
+        //Usuario usuario = usuarioService.find(idUsu);
+        List<AreaConhecimento> areasConhecimento;
+        areasConhecimento = areaConhecimentoService.findAll();
+
+        model.addAttribute("areasConhecimento", areasConhecimento);
+        //model.addAttribute("usuario", usuario);
+
+        return "areas-conhecimento";
+    }
+}
