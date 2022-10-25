9651d758be153914dc6e5c71b58de6c67c830bd6
ewerton.barbosa <ewerton.barbosa@avmb-asten.com.br>
2022-10-25 10:19:03 -0300

    inserido método para trazer as cartas de uma área de conhecimento.

--- src/main/java/br/ufsm/tcc2/pmmaster/repository/CartaRepository.java
+++ src/main/java/br/ufsm/tcc2/pmmaster/repository/CartaRepository.java
@@ -1,7 +1,12 @@
 package br.ufsm.tcc2.pmmaster.repository;
 
+import br.ufsm.tcc2.pmmaster.model.AreaConhecimento;
 import br.ufsm.tcc2.pmmaster.model.Carta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CartaRepository extends JpaRepository <Carta, Long> {
+
+    List<Carta> findByAreaConhecimento(AreaConhecimento areaConhecimento);
 }
