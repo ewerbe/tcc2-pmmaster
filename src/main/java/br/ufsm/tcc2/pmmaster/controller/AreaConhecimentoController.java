8aea0dc41405993b436d4231d79ee83f7a987c3c
ewerton.barbosa <ewerton.barbosa@avmb-asten.com.br>
2022-10-25 10:17:09 -0300

    inserido método para trazer as cartas de uma área de conhecimento.

--- src/main/java/br/ufsm/tcc2/pmmaster/controller/AreaConhecimentoController.java
+++ src/main/java/br/ufsm/tcc2/pmmaster/controller/AreaConhecimentoController.java
@@ -1,12 +1,15 @@
 package br.ufsm.tcc2.pmmaster.controller;
 
 import br.ufsm.tcc2.pmmaster.model.AreaConhecimento;
+import br.ufsm.tcc2.pmmaster.model.Carta;
 import br.ufsm.tcc2.pmmaster.service.AreaConhecimentoService;
+import br.ufsm.tcc2.pmmaster.service.CartaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,6 +18,8 @@ public class AreaConhecimentoController {
 
     @Autowired
     AreaConhecimentoService areaConhecimentoService;
+    @Autowired
+    CartaService cartaService;
 
     @RequestMapping(value = "/areas-conhecimento.action", method = RequestMethod.GET)
     public String getAreasConhecimento(Model model) {
@@ -27,5 +32,13 @@ public class AreaConhecimentoController {
         //model.addAttribute("usuario", usuario);
 
         return "areas-conhecimento";
+    }
+
+    @RequestMapping(value = "/areas-conhecimento/cartas-area-conhecimento.action", method = RequestMethod.GET)
+    public String getCartasAreaConhecimento(Model model, @RequestParam(value = "id")Long idAreaConhecimento){
+        List<Carta> cartas = cartaService.findByAreaConhecimento(idAreaConhecimento);
+
+        model.addAttribute("cartas", cartas);
+        return "cartas";
     }
 }
