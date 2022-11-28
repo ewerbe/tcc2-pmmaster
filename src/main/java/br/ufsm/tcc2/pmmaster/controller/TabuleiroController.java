6070377acddfbba46e63d2f729dfb2f3abf125f7
Ewe Barbo <ewebarbo@gmail.com>
2022-11-28 14:37:20 -0300

    método para teste de inserção de tabuleiro jasper na aplicação inicializado.

--- src/main/java/br/ufsm/tcc2/pmmaster/controller/TabuleiroController.java
+++ src/main/java/br/ufsm/tcc2/pmmaster/controller/TabuleiroController.java
@@ -1,21 +1,25 @@
 package br.ufsm.tcc2.pmmaster.controller;
 
-import br.ufsm.tcc2.pmmaster.model.Carta;
 import br.ufsm.tcc2.pmmaster.model.Tabuleiro;
 import br.ufsm.tcc2.pmmaster.service.TabuleiroService;
+import br.ufsm.tcc2.pmmaster.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
 public class TabuleiroController {
 
     @Autowired
-    TabuleiroService tabuleiroService;
+    private TabuleiroService tabuleiroService;
+    @Autowired
+    private UsuarioService usuarioService;
 
     @RequestMapping(value = "/tabuleiros.action", method = RequestMethod.GET)
     public String getTabuleiros(Model model) {
@@ -28,5 +32,31 @@ public class TabuleiroController {
         //model.addAttribute("usuario", usuario);
 
         return "tabuleiros";
+    }
+
+    @RequestMapping(value = "/tabuleiros/novo-tabuleiro.action", method = RequestMethod.POST)
+    public String salvarCarta(Model model, HttpServletRequest request,
+                              @RequestParam(value = "idUsuario", required = false)Long idUsuario){
+
+        //TODO: teste com ID_TABULEIRO fixo, para por o tabueleiro Jasper na webapp;
+        Long idTabuleiro = 1L;
+
+        Tabuleiro tabuleiroNovo = new Tabuleiro();
+        tabuleiroNovo.setId(idTabuleiro);
+        tabuleiroNovo.setIdUsuario(usuarioService.find(idUsuario));
+
+        //consistir o novo tabuleiro no banco;
+        tabuleiroService.save(tabuleiroNovo);
+        //criar o novo arquivo (jasper) do tabuleiro, passando o idUsuario pra gerar a URL destino do QRCODE;
+        Boolean tabuleiroGerado = geraArquivoTabuleiro(tabuleiroNovo.getIdUsuario().getId());
+
+        return "redirect:/tabuleiros.action";
+    }
+
+    ////////////////////////////
+
+    private Boolean geraArquivoTabuleiro(Long idUsuario) {
+        //chamar métodos Jasper de criação do tabuleiro.pdf;
+        return true;
     }
 }
