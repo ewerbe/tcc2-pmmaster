8e9986c38a7087996b85c156bc2af8d3a4bdb48a
Ewe Barbo <ewebarbo@gmail.com>
2022-12-06 10:12:51 -0300

    Refatorado para teste realizado com sucesso.

--- src/main/java/br/ufsm/tcc2/pmmaster/controller/TabuleiroController.java
+++ src/main/java/br/ufsm/tcc2/pmmaster/controller/TabuleiroController.java
@@ -5,17 +5,24 @@ import br.ufsm.tcc2.pmmaster.model.Usuario;
 import br.ufsm.tcc2.pmmaster.service.TabuleiroReportService;
 import br.ufsm.tcc2.pmmaster.service.TabuleiroService;
 import br.ufsm.tcc2.pmmaster.service.UsuarioService;
-import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.repo.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.List;
+import java.io.IOException;
+import java.util.*;
 
 @Controller
 public class TabuleiroController {
@@ -30,43 +37,82 @@ public class TabuleiroController {
     @RequestMapping(value = "/tabuleiros.action", method = RequestMethod.GET)
     public String getTabuleiros(Model model,
                                    @RequestParam(value = "idUsuario", required = false)Long idUsuario) {
-        Usuario usuario = usuarioService.find(idUsuario);
+        //Usuario usuario = usuarioService.find(idUsuario);
         List<Tabuleiro> tabuleiros;
         tabuleiros = tabuleiroService.findAll();
 
         model.addAttribute("tabuleiros", tabuleiros);
-        model.addAttribute("idUsuario", usuario.getId());
+        model.addAttribute("idUsuario", 1L);
 
         return "tabuleiros";
     }
 
     @RequestMapping(value = "/tabuleiros/novo-tabuleiro.action", method = RequestMethod.POST)
-    public String salvarTabuleiro(Model model, HttpServletRequest request,
-                              @RequestParam(value = "idUsuario", required = false)Long idUsuario) throws JRException, FileNotFoundException {
+    public void gera(Model model,
+                                  HttpServletRequest request,
+                                  HttpServletResponse response,
+                                  @RequestParam(value = "idUsuario", required = false)Long idUsuario) throws Exception {
 
         //TODO: teste com ID_TABULEIRO fixo, para por o tabuleiro Jasper na webapp;
         //Long idTabuleiro = 1L;
 
-        Tabuleiro tabuleiroNovo = new Tabuleiro();
+        //Tabuleiro tabuleiroNovo = new Tabuleiro();
         //tabuleiroNovo.setId(idTabuleiro);
-        tabuleiroNovo.setIdUsuario(usuarioService.find(idUsuario));
+        //tabuleiroNovo.setIdUsuario(usuarioService.find(idUsuario));
 
         //consistir o novo tabuleiro no banco;
-        tabuleiroService.save(tabuleiroNovo);
+        //tabuleiroService.save(tabuleiroNovo);
         //criar o novo arquivo (jasper) do tabuleiro, passando o idUsuario pra gerar a URL destino do QRCODE;
 //        Boolean tabuleiroGerado = geraArquivoTabuleiro(tabuleiroNovo.getIdUsuario().getId());
-        tabuleiroReportService.exportReport(tabuleiroNovo.getId());
-        return "redirect:/tabuleiros.action";
+        //tabuleiroReportService.exportReport(tabuleiroNovo.getId());
+
+        response.setContentType("application/pdf");
+        response.addHeader("Content-Disposition", "attachment; filename=tabuleiro.pdf");
+
+        //geraArquivoTabuleiro(tabuleiroNovo.getId());
+        response.getOutputStream().write(geraArquivoTabuleiro(6L));
     }
 
     ////////////////////////////
 
-    private Boolean geraArquivoTabuleiro(Long idUsuario) {
-        //todo: Boa ideia em AbstractRelatorio Service.java na lib "relatorios" do Portal de Relatorios;
-        //TODO: método: geraRelatorio();
-        //chamar métodos Jasper de criação do tabuleiro.pdf;
-         //arquivoTabuleiro;
+    private byte[] geraArquivoTabuleiro(Long idTabuleiro) throws FileNotFoundException, JRException {
 
-        return true;
+        //Tabuleiro tabuleiro = tabuleiroService.find(idTabuleiro);
+        //File file = ResourceUtils.getFile("classpath:tabuleiro.jrxml");
+        File file = ResourceUtils.getFile("classpath:tabuleiro.jrxml");
+        //File file = ResourceUtils.getFile("classpath:src/main/resources/tabuleiro.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(null);
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("ID_TABULEIRO", idTabuleiro);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
+
+        return JasperExportManager.exportReportToPdf(jasperPrint);
     }
+
+
+    private JasperPrint getJasperPrint(final String jrxmlName,
+                                       final Map<String, Object> params,
+                                       final Collection<?> beanCollection) throws Exception {
+        final File file = ResourceUtils.getFile("classpath:reports//" + jrxmlName);
+        final DefaultJasperReportsContext contexto = DefaultJasperReportsContext.getInstance();
+        final JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(beanCollection);
+        final JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        addDefaultParams(params);
+        return JasperFillManager.fillReport(jasperReport, params, dataSource);
+    }
+
+    private void addDefaultParams(final Map<String, Object> params) throws FileNotFoundException {
+        final File fileImg = ResourceUtils.getFile("classpath:static//assets//img//imagemxxx.png");
+        params.put("SRC_IMAGE", fileImg.getAbsolutePath());
+    }
+
+    public byte[] exportPdf(final String jrxmlName,
+                            final Map<String, Object> params,
+                            final Collection<?> beanCollection) throws Exception {
+        final JasperPrint jasperPrint = getJasperPrint(jrxmlName, params, beanCollection);
+        return JasperExportManager.exportReportToPdf(jasperPrint);
+    }
+
+
 }
